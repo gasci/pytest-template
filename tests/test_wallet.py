3a96d50bbb939e2fde49a787acf0df9e52f3d666
Göktuğ Aşcı <g.asci@hotmail.com>
2022-04-23 12:14:43 +0200

    add multiple fixtures

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -13,7 +13,7 @@ def empty_wallet():
 
 
 @pytest.fixture
-def wallet():
+def wallet_20():
     """Returns a Wallet instance with a balance of 20"""
     return Wallet(20)
 
@@ -22,18 +22,20 @@ def test_default_initial_amount(empty_wallet):
     assert empty_wallet.balance == 0
 
 
-def test_setting_initial_amount(wallet):
-    assert wallet.balance == 20
+def test_setting_initial_amount(wallet_20):
+    assert wallet_20.balance == 20
 
 
-def test_wallet_add_cash(wallet):
-    wallet.add_cash(80)
-    assert wallet.balance == 100
+def test_wallet_spend_cash(wallet_20):
+    wallet_20.spend_cash(10)
+    assert wallet_20.balance == 10
 
 
-def test_wallet_spend_cash(wallet):
-    wallet.spend_cash(10)
-    assert wallet.balance == 10
+# multiple fixtures
+def test_wallet_add_cash(wallet_20, empty_wallet):
+    wallet_20.add_cash(80)
+    empty_wallet.add_cash(80)
+    assert empty_wallet.balance == 80 and wallet_20.balance == 100
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
@@ -41,10 +43,13 @@ def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet)
         empty_wallet.spend_cash(100)
 
 
-@pytest.mark.parametrize("earned,spent,expected", [
-    (30, 10, 20),
-    (20, 2, 18),
-])
+@pytest.mark.parametrize(
+    "earned,spent,expected",
+    [
+        (30, 10, 20),
+        (20, 2, 18),
+    ],
+)
 def test_transactions(empty_wallet, earned, spent, expected):
     empty_wallet.add_cash(earned)
     empty_wallet.spend_cash(spent)
